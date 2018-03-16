c5c15ce716a49743b294a020386abddc0d73c64b
togacoder <bike.dio@gmail.com>
2018-03-16 23:48:06 +0900

    A - Product

---
+++ abc086a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if((a % 2 == 1)&&(b % 2 == 1)) {
+		cout << "Odd" << endl;
+	} else {
+		cout << "Even" << endl;
+	}
+	
+	return 0;
+}
