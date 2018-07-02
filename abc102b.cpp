f8322d1ad86dedb43d7b2a350ebacac34dcab93d
togacoder <bike.dio@gmail.com>
2018-07-02 22:52:23 +0900

    B - Maximum Difference

---
+++ abc102b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    cout << abs(arr[n - 1] - arr[0]) << endl;
+        
+    return 0;
+}
