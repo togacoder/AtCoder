2e9bfce9d3c6ce5edaec5f673e16817c2d8f3bdd
togacoder <bike.dio@gmail.com>
2018-09-11 00:39:02 +0900

    A - Task Scheduling Problem

---
+++ abc103a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> arr(3);
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr.begin(), arr.end());
+    cout << (arr[1] - arr[0]) + (arr[2] - arr[1]) << endl;
+
+    return 0;
+}
